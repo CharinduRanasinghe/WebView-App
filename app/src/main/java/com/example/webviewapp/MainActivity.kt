a1c7768875e5b1e7018a6715b3f9fb54f70d2911
Charindu Ranasinghe <charindu2001@gmail.com>
2024-10-15 13:04:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.webviewapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+
+        // Enable JavaScript for the WebView
+        webView.settings.javaScriptEnabled = true
+
+        // Load the URL
+        webView.loadUrl("http://sparcica.local:5000")
+    }
+
+    // Handle back button to go back in WebView history
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
